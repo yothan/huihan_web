27a6bdee8c724fc4230f24dd5b8cc122c651b8a0
Ahan <yothan511@gmail.com>
2024-02-06 17:46:08 +0800

    修改优化一些样式

--- scripts/first_level_page.js
+++ scripts/first_level_page.js
@@ -6,16 +6,16 @@ $(function(){
 //顶部导航滚动时 “背景透明度&阴影” 映射
 $(document).scroll(function(){
   var a1 = $(document).scrollTop();
-  var b1 = a1 / 200;
+  var b1 = a1 / 300;
   if( b1 <= 1){
-    c1 = b1*0.96;//*0.4+0.56;
-    c2 = b1*0.2;
+    c1 = b1*0.8;//*0.4+0.56;
+    c2 = b1*0.06;
   }else{
-    c1 = 0.96;//0.96;
-    c2 = 0.2;
+    c1 = 0.8;//0.96;
+    c2 = 0.06;
   }
   var d1 = "rgba(255, 255, 255," + c1 + ")";
-  var d2 = "0px 0px 4px rgba(0, 0, 0," + c2 + ")";
+  var d2 = "0px 4px 8px rgba(0, 0, 0," + c2 + ")";
   $('div.nav').css('background-color',d1);
   $('div.nav').css('box-shadow',d2);
 //  alert(d1);
